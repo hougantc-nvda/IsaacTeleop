9d24cd1843ddf3fc5583e7b5ae97ca17cd5a6043
Sergey Grizan <sgrizan@nvidia.com>
2026-04-14 11:11:28 -0700

    ROS: Publish raw controller commands in teleop mode (#393)

--- examples/teleop_ros2/python/teleop_ros2_node.py
+++ examples/teleop_ros2/python/teleop_ros2_node.py
@@ -857,7 +857,11 @@ class TeleopRos2Node(Node):
                                 ).tolist()
                                 self._pub_finger_joints.publish(finger_joints_msg)
 
-                        if self._mode in ("controller_raw", "full_body"):
+                        if self._mode in (
+                            "controller_raw",
+                            "controller_teleop",
+                            "full_body",
+                        ):
                             if not left_ctrl.is_none or not right_ctrl.is_none:
                                 controller_payload = _build_controller_payload(
                                     left_ctrl, right_ctrl
